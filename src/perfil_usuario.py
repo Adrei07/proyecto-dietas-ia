e8e5ac840e75948716418c1c01b7992893d50218
Adrei07 <adrei.blanco@gmail.com>
2026-04-16 15:20:08 -0600

    Implement user profile class with validation
    In this phase, the initial structure of the system is developed using object-oriented programming. The PerfilUsuario class is implemented to store user data and calculate their BMI along with its corresponding category.
    
    Additionally, the PlanDieta class is introduced to represent a personalized diet, including key attributes such as calories and macronutrients. This phase also prepares the foundation for the future implementation of a diet recommendation system.
    
    Overall, this phase establishes the core structure, allowing the creation of user profiles and the assignment of an initial diet.

--- src/perfil_usuario.py
+++ src/perfil_usuario.py
@@ -1,1 +1,70 @@
+class PerfilUsuario: 
+  def __init__(self, nombre, edad, sexo, peso, altura, nivel_actividad, objetivo, alergias, no_deseados):
+    if not isinstance(nombre, str):
+        raise TypeError("nombre debe ser del tipo str")
+    if not isinstance(edad, int):
+        raise TypeError("nombre debe ser del tipo str")
+    if edad<0 and edad>130: 
+        raise ValueError("La edad debe de estar entre 0 y 130")
+    if not isinstance(sexo, str):
+        raise TypeError("nombre debe ser del tipo str")
+    if not isinstance(sexo, str):
+        raise TypeError("nombre debe ser ")
+    if not isinstance(peso, (int, float)):
+         raise TypeError("El peso debe ser un número unicamente")
+    if not isinstance(altura, (int, float)):
+        raise TypeError("La altura debe ser un número")
+    if not isinstance(alergias, list):
+        raise TypeError("Las alergias deben ser una lista") 
+    if not isinstance(no_deseados, list):
+         raise TypeError("Los alimentos no deseados deben ser una lista")
+    
+    self.nombre = nombre 
+    self.edad = edad 
+    self.sexo = sexo 
+    self.peso = peso 
+    self.altura = altura 
+    self.nivel_actividad = nivel_actividad
+    self.objetivo = objetivo
+    self.alergias = alergias
+    self.no_deseados = no_deseados
+    
+  @property
+  def imc(self): 
+      if self.altura == 0: #En caso de que la altura sea 0 retornamos 0 para evitar invalidación
+          return 0
+      imc = self.peso/self.altura**2
+      return imc 
 
+  def interpretar_imc(self): 
+    
+      if self.imc<18.5: 
+          return "bajo"
+      elif self.imc<25: 
+          return "normal"
+      elif self.imc<30: 
+          return "sobre peso"
+      else:
+          return "obesidad"
+  
+  def mostrar_perfil(self): 
+      print(f"Nombre: {self.nombre}")
+      print(f"Edad: {self.edad}")
+      print(f"Sexo: {self.sexo}")
+      print(f"Peso: {self.peso} kg")
+      print(f"Altura: {self.altura} m")
+      print(f"Nivel de actividad: {self.nivel_actividad}")
+      print(f"Objetivo: {self.objetivo}")
+      print(f"Alergias: {", ".join(self.alergias)}")
+      print(f"Alimentos no deseados: {", ".join(self.no_deseados)}")
+      print(f"IMC: {round(self.imc,3)}")
+      print(f"Categoria de IMC: {self.interpretar_imc()}")
+      
+    
+class Dieta: 
+    pass 
+      
+      
+      
+Adrei = PerfilUsuario("Adrei", 19, "Masculino", 68, 1.68, "Alto", "Ganar masa muscular", ["Ninguna "], ["Brocoli","Coliflor","Pancita"])
+Adrei.mostrar_perfil()
