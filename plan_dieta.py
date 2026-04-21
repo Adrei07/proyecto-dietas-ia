b70e13f350f4aa830de974a3ab205c7d684bb3e5
Adrei07 <adrei.blanco@gmail.com>
2026-04-21 13:53:19 -0600

    Add PerfilUsuario class for user profile management
    Add @property for BMI to avoid recalculating value

--- plan_dieta.py
+++ plan_dieta.py
@@ -1,31 +1,62 @@
-class RegistrarDieta: 
-    def __init__(self,nombre_dieta:str,calorias:int|float, proteinas:int|float, carbohidratos:int|float,grasas:int|float, descripcion:str):
-        
-        if not isinstance(nombre_dieta, str):
-            raise TypeError("El nombre de la dieta debe ser texto")
-        if not isinstance(calorias, (int, float)):
-            raise TypeError("Las calorías deben ser un número")
-        if not isinstance(proteinas, (int, float)):
-            raise TypeError("Las proteínas deben ser un número")
-        if not isinstance(carbohidratos, (int, float)):
-            raise TypeError("Los carbohidratos deben ser un número")
-        if not isinstance(grasas, (int, float)):
-            raise TypeError("Las grasas deben ser un número")
-        if not isinstance(descripcion, str):
-            raise TypeError("La descripción debe ser texto")
-            
-        self.nombre_dieta = nombre_dieta
-        self.calorias = calorias
-        self.proteinas = proteinas
-        self.carbohidratos = carbohidratos
-        self.grasas = grasas
-        self.descripcion = descripcion
-        
-    def mostrar_dieta(self):
-        print("\n====Plan dieta=====\n")
-        print(f"Nombre: {self.nombre_dieta}")
-        print(f"Calorías: {self.calorias}kcal")
-        print(f"Proteínas: {self.proteinas} g")
-        print(f"Carbohidratos: {self.carbohidratos} g")
-        print(f"Grasas: {self.grasas} g")
-        print(f"Descripción: {self.descripcion}")
+class PerfilUsuario: 
+  def __init__(self, nombre, edad, sexo, peso, altura, nivel_actividad, objetivo, alergias, no_deseados):
+    if not isinstance(nombre, str):
+        raise TypeError("nombre debe ser del tipo str")
+    if not isinstance(edad, int):
+        raise TypeError("nombre debe ser del tipo str")
+    if edad<0 and edad>130: 
+        raise ValueError("La edad debe de estar entre 0 y 130")
+    if not isinstance(sexo, str):
+        raise TypeError("nombre debe ser del tipo str")
+    if not isinstance(sexo, str):
+        raise TypeError("nombre debe ser ")
+    if not isinstance(peso, (int, float)):
+         raise TypeError("El peso debe ser un número unicamente")
+    if not isinstance(altura, (int, float)):
+        raise TypeError("La altura debe ser un número")
+    if not isinstance(alergias, list):
+        raise TypeError("Las alergias deben ser una lista") 
+    if not isinstance(no_deseados, list):
+         raise TypeError("Los alimentos no deseados deben ser una lista")
+    
+    self.nombre = nombre 
+    self.edad = edad 
+    self.sexo = sexo 
+    self.peso = peso 
+    self.altura = altura 
+    self.nivel_actividad = nivel_actividad
+    self.objetivo = objetivo
+    self.alergias = alergias
+    self.no_deseados = no_deseados
+    
+  @property
+  def imc(self): 
+      if self.altura == 0: #En caso de que la altura sea 0 retornamos 0 para evitar invalidación
+          return 0
+      imc = self.peso/self.altura**2
+      return imc 
+
+  def interpretar_imc(self): 
+    
+      if self.imc<18.5: 
+          return "bajo"
+      elif self.imc<25: 
+          return "normal"
+      elif self.imc<30: 
+          return "sobre peso"
+      else:
+          return "obesidad"
+  
+  def mostrar_perfil(self): 
+      print("====Perfil del Usuario====\n")
+      print(f"Nombre: {self.nombre}")
+      print(f"Edad: {self.edad}años")
+      print(f"Sexo: {self.sexo}")
+      print(f"Peso: {self.peso}kg")
+      print(f"Altura: {self.altura}m")
+      print(f"Nivel de actividad: {self.nivel_actividad}")
+      print(f"Objetivo: {self.objetivo}")
+      print(f"Alergias: {", ".join(self.alergias)}")
+      print(f"Alimentos no deseados: {", ".join(self.no_deseados)}")
+      print(f"IMC: {round(self.imc,3)}")
+      print(f"Categoria de IMC: {self.interpretar_imc()}")
